315aaa52c46fcc296e376ffed5a38f523f1db84d
pxy <252584841@qq.com>
2019-12-16 16:39:09 +0800

    a

--- public/js/reg.js
+++ public/js/reg.js
@@ -77,10 +77,12 @@ $('.red_button').click(function () {
     // });
 
     $.get("http://127.0.0.1:3000/api/reg", {
-      user1: $(".reg_user").val(),
+      user: $(".reg_user").val(),
       password: $(".reg_password").val(),
       email: $(".reg_email").val(),
-      mobile: $(".reg_mobile").val()
+      mobile: $(".reg_mobile").val(),
+      create_time: dateFtt("yyyy-MM-dd hh:mm:ss",new Date()),
+      remark: "aaaaa"
     },
 
       function (data, textStatus, fn) {
@@ -96,3 +98,22 @@ $('.red_button').click(function () {
     alert("请完善信息");
   }
 });
+
+
+function dateFtt(fmt, date) { //author: meizz 
+  var o = {
+    "M+": date.getMonth() + 1,     //月份 
+    "d+": date.getDate(),     //日 
+    "h+": date.getHours(),     //小时 
+    "m+": date.getMinutes(),     //分 
+    "s+": date.getSeconds(),     //秒 
+    "q+": Math.floor((date.getMonth() + 3) / 3), //季度 
+    "S": date.getMilliseconds()    //毫秒 
+  };
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt))
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+}
